9a7717fa819f3848313bbe18a2e1d85233b64f24
tolik <lanot.biz@gmail.com>
2026-05-26 20:58:58 +0300

    added path visualized as cv::Mat::zeros

--- include/opencv_visualizer.hpp
+++ include/opencv_visualizer.hpp
@@ -4,7 +4,16 @@
 
 class OpenCvVisualizer : public IVisualizer
 {
+private:
+    cv::Mat trajectory_;
+    cv::Point previous_point_;
+    bool first_pose_ = true;
+    int trajectory_rows_px_ = 1000;
+    int trajectory_cols_px_ = 1800;
+
 public:
+    OpenCvVisualizer();
+
     void drawMatches(
         Frame::Ptr prev,
         Frame::Ptr curr,
