b7d636670f608e366bbdb8cfcf1fd327e673bca4
tolik <lanot.biz@gmail.com>
2026-05-26 20:58:54 +0300

    added path visualized as cv::Mat::zeros

--- src/opencv_visualizer.cpp
+++ src/opencv_visualizer.cpp
@@ -1,6 +1,15 @@
 #include "opencv_visualizer.hpp"
-
 #include <iostream>
+
+OpenCvVisualizer::OpenCvVisualizer()
+{
+    trajectory_ =
+        cv::Mat::zeros(
+            trajectory_rows_px_,
+            trajectory_cols_px_,
+            CV_8UC3
+        );
+}
 
 void OpenCvVisualizer::drawMatches(
     Frame::Ptr prev,
@@ -29,10 +38,34 @@ void OpenCvVisualizer::drawMatches(
 void OpenCvVisualizer::drawPose(
     const Sophus::SE3d& pose)
 {
-    Eigen::Vector3d t = pose.translation();
+    Eigen::Vector3d t =
+        pose.translation();
 
-    std::cout
-        << "Pose: "
-        << t.transpose()
-        << std::endl;
+    std::cout << "POSE: X: " << t.x() << "  Y: " << t.y() << std::endl;
+
+    int x = static_cast<int>(t.x() * 1.5) + static_cast<int>(trajectory_cols_px_ / 2);
+    int y = static_cast<int>(t.y() * 1.5) + static_cast<int>(trajectory_rows_px_ / 2);
+
+    cv::Point current_point(x, y);
+
+    if (!first_pose_)
+    {
+        cv::line(
+            trajectory_,
+            previous_point_,
+            current_point,
+            cv::Scalar(0, 255, 0),
+            2
+        );
+    }
+
+    previous_point_ = current_point;
+    first_pose_ = false;
+
+    cv::imshow(
+        "Trajectory",
+        trajectory_
+    );
+
+    cv::waitKey(1);
 }
